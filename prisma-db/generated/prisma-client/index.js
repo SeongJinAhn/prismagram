c68bb8d6b9b56994624478fd7a1a170c0c84b8b5
SeongJinAhn <sja1425@naver.com>
2019-08-23 19:25:20 +0900

    connect prisma

---
+++ prisma-db/generated/prisma-client/index.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Post",
+    embedded: false
+  },
+  {
+    name: "Like",
+    embedded: false
+  },
+  {
+    name: "Comment",
+    embedded: false
+  },
+  {
+    name: "File",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/sssjjjaaa-d7b646/prisma-db/dev`
+});
+exports.prisma = new exports.Prisma();
